93e96a175ff196ee29477e73b3c576aa770bb80c
Mariosep <mariosep28@gmail.com>
2018-10-22 21:18:02 +0200

    hola

--- encuesta.php
+++ encuesta.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="encuesta.css">
+	<link rel="stylesheet" href="encuesta-style.css">
 </head>
 
 <body>
@@ -26,7 +26,7 @@
 	<section>
 		<div class="row">
 			<div class="col-3">
-				<h6>ASIGNATURA</h6>
+				<h6 id="title-asignatura">ASIGNATURA</h6>
 				<select>
 			    	<option value="1">Matemática Discreta</option>
 				    <option value="2">Introducción a la Programación</option>
@@ -35,7 +35,7 @@
 
 			</div>
 			<div class="col-6">
-				<h6 id="txt-pregUs">INFORMACIÓN PERSONAL Y ACADÉMICA DE LOS ESTUDIANTES</h6>
+				<h6 id="title-pregUs">INFORMACIÓN PERSONAL Y ACADÉMICA DE LOS ESTUDIANTES</h6>
 				<div class="preguntas-us">
 					<form action="">
 					  <table>
@@ -78,112 +78,57 @@
 					</table>
 				</div>
 			</div
-
-
+				
 		<div class="row">
 			<table class="valoraciones-prof">
 			  	<tr>
 			  		<th class="enunciado-prof">
-				  		<h6 class="title-topic">PLANIFICACIÓN DE LA ENSEÑANZA Y APRENDIZAJE</h6> <br>
-				  	  	<span>1. El/la profesor/a informa sobre los distintos aspectos de la guía docente o programa de la asignatura (objetivos, actividades, contenidos del temario, metodología, bibliografía, sistemas de evaluación,...)</span>		
+				  		<h6 class="title-topic">PLANIFICACIÓN DE LA ENSEÑANZA Y APRENDIZAJE</h6>
+				  	  	<span class="enunciado">1. El/la profesor/a informa sobre los distintos aspectos de la guía docente o programa de la asignatura (objetivos, actividades, contenidos del temario, metodología, bibliografía, sistemas de evaluación,...)</span>		
 					</th>
-					<div class="valoracion">
-						<th>
-						NS <br> <input type="radio" name="1" value="1"> 
-						</th>
-						<th>  
-						1 <br> <input type="radio" name="1" value="2"> 
-					    </th>
-						<th>  
-						2 <br>  <input type="radio" name="1" value="3">
-						</th>
-						<th>  
-						3 <br>  <input type="radio" name="1" value="4">
-						</th>
-						<th>  
-						4 <br>  <input type="radio" name="1" value="5">
-						</th>
-						<th>  
-						5 <br>  <input type="radio" name="1" value="5">
-						</th>
-					</div>
+					<th>
+						NS <input type="radio" name="1" value="1"> 
+					</th>
+					<th>  
+						1 <input type="radio" name="1" value="2"> 
+				    </th>
+					<th>  
+						2  <input type="radio" name="1" value="3">
+					</th>
+					<th>  
+						3  <input type="radio" name="1" value="4">
+					</th>
+					<th>  
+						4  <input type="radio" name="1" value="5">
+					</th>
+					<th>  
+						5  <input type="radio" name="1" value="5">
+					</th>
 				</tr>
 				<tr>
 				  	<th class="enunciado-prof">
 				  		<h6 class="title-topic">DESARROLLO DE LA DOCENCIA</h6>
 		  				<h7 class="title-subtopic">Cumplimiento de las obligaciones docentes (del encargo docente)</h7> <br>
-				  	  	<span>2. Imparte las clases en el horario fijado</span>		
+				  	  	<span class="enunciado">2. Imparte las clases en el horario fijado</span>		
 					</th>
-					<div class="valoracion">
-						<th>
-						NS <br> <input type="radio" name="1" value="1"> 
-						</th>
-						<th>  
-						1 <br> <input type="radio" name="1" value="2"> 
-					    </th>
-						<th>  
-						2 <br>  <input type="radio" name="1" value="3">
-						</th>
-						<th>  
-						3 <br>  <input type="radio" name="1" value="4">
-						</th>
-						<th>  
-						4 <br>  <input type="radio" name="1" value="5">
-						</th>
-						<th>  
-						5 <br>  <input type="radio" name="1" value="5">
-						</th>
-					</div>
-				</tr>
-				<tr>
-				  	<th class="enunciado-prof">
-				  	  	<span>3. Asiste regularmente a clase</span>		
+					<th>
+						NS <input type="radio" name="1" value="1"> 
 					</th>
-					<div class="valoracion">
-						<th>
-						NS <br> <input type="radio" name="1" value="1"> 
-						</th>
-						<th>  
-						1 <br> <input type="radio" name="1" value="2"> 
-					    </th>
-						<th>  
-						2 <br>  <input type="radio" name="1" value="3">
-						</th>
-						<th>  
-						3 <br>  <input type="radio" name="1" value="4">
-						</th>
-						<th>  
-						4 <br>  <input type="radio" name="1" value="5">
-						</th>
-						<th>  
-						5 <br>  <input type="radio" name="1" value="5">
-						</th>
-					</div>
-				</tr>
-				<tr>
-				  	<th class="enunciado-prof">
-				  	  	<span>4. Cumple adecuadamente su labor de tutoría (presencial o virtual)</span>		
+					<th>  
+						1 <input type="radio" name="1" value="2"> 
+				    </th>
+					<th>  
+						2  <input type="radio" name="1" value="3">
 					</th>
-					<div class="valoracion">
-						<th>
-						NS <br> <input type="radio" name="1" value="1"> 
-						</th>
-						<th>  
-						1 <br> <input type="radio" name="1" value="2"> 
-					    </th>
-						<th>  
-						2 <br>  <input type="radio" name="1" value="3">
-						</th>
-						<th>  
-						3 <br>  <input type="radio" name="1" value="4">
-						</th>
-						<th>  
-						4 <br>  <input type="radio" name="1" value="5">
-						</th>
-						<th>  
-						5 <br>  <input type="radio" name="1" value="5">
-						</th>
-					</div>
+					<th>  
+						3  <input type="radio" name="1" value="4">
+					</th>
+					<th>  
+						4  <input type="radio" name="1" value="5">
+					</th>
+					<th>  
+						5  <input type="radio" name="1" value="5">
+					</th>
 				</tr>
 			</table>
 		</div>
